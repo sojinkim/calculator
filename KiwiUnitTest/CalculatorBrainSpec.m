8d3f484c13c5ed56bb1b64298708ce1356e6d987
Sojin Kim <sojinkim@Sojin-ui-MacBook-Air.local>
2013-02-14 01:28:08 +0900

    kiwi setup

---
+++ KiwiUnitTest/CalculatorBrainSpec.m
@@ -0,0 +1,27 @@
+#import "CalculatorBrain.h"
+#import "Kiwi.h"
+
+SPEC_BEGIN(CalculatorBrainSpec)
+
+describe(@"CalculatorBrainSpec", ^{
+    context(@"brain is in initial state", ^{
+        __block CalculatorBrain *myBrain = nil;
+        
+        beforeEach(^{
+            myBrain = [[CalculatorBrain alloc] init];
+            [myBrain initialize];
+        });
+        
+        afterEach(^{
+            myBrain = nil;
+        });
+        
+        it(@"should ignore enter and stay in the initial state", ^{
+           [myBrain processEnter];
+           [[theValue([myBrain currentState]) should] equal:theValue(brainState_init)];
+           });
+    });
+});
+
+
+SPEC_END
